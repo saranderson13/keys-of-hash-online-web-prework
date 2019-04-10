d586838ab4748c54d13dcb8428712b5088cace2a
Sarah Anderson <saranderson13@gmail.com>
2019-04-10 16:24:00 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,30 @@
+require 'pry'
+
+animals_test = {
+  "sugar glider"=>"Australia",
+  "aye-aye"=> "Madagascar",
+  "red-footed tortoise"=>"Panama",
+  "kangaroo"=> "Australia",
+  "tomato frog"=>"Madagascar",
+  "koala"=>"Australia"
+}
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    # returns an array of every key with a value that matches an argument
+    
+    array_of_keys = []
+    
+    arguments.each do |arg|
+      values.each do |value|
+        if key(arg) == value
+          array_of_keys << key(arg)
+          # binding.pry
+        end
+      end
+    end
+    array_of_keys
+    # binding.pry
   end
 end
+# animals_test.keys_of('Madagascar', 'Panama')
